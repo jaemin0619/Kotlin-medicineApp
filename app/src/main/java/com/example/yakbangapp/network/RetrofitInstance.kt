508b14ce16f92693b2bda0b6d2c0eb7fe54e5460
jaemin0619 <wjdwoals000619@naver.com>
2024-08-08 18:31:02 +0900

    last upload

--- app/src/main/java/com/example/yakbangapp/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/yakbangapp/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService"
+    private const val BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
